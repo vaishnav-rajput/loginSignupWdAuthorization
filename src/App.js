befbaa2cc806cafae22c78339fdfefe94b22a2fa
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-07-07 22:04:16 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/App.css";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <main className="App">
+      <Register/> 
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,17 @@
+import {useRef, useState, useEffect} from "react"
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react'
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGX = /^(?=. *[A-Z])(?)
+
+const Register = () => {
+ 
+
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
